01811d0f953ba9cf010513f67ad0d7a2a1bea1a4
Jeremy Miles <jeremy.miles@gmail.com>
2025-06-08 22:46:23 -0700

    First commit

---
+++ CatMA.R
@@ -0,0 +1,221 @@
+
+CatMA <- function(
+    data = data,
+    label = "ID",
+    outcome,
+    comparator_analysis = TRUE,
+    comparator_vs_int = comparator_vs_int
+) {
+  
+  # Can't do both!
+  if (comparator_vs_int) {
+    comparator_analysis <- FALSE
+  }
+  
+  data$ai <- data[[glue::glue("{categorical_outcome}_counts_ctrl_categ")]]
+  data$bi <- 
+    data[[glue::glue("{categorical_outcome}_n_ctrl_categ")]] - 
+    data[[glue::glue("{categorical_outcome}_counts_ctrl_categ")]]
+  data$ci <- data[[glue::glue("{categorical_outcome}_counts_int_categ")]]
+  data$di <- data[[glue::glue("{categorical_outcome}_n_int_categ")]] - 
+    data[[glue::glue("{categorical_outcome}_counts_int_categ")]] 
+  
+  if (comparator_vs_int) {
+    data$ai <- data[[glue::glue("{categorical_outcome}_counts_ctrl_categ")]]
+    data$bi <- 
+      data[[glue::glue("{categorical_outcome}_n_ctrl_categ")]] - 
+      data[[glue::glue("{categorical_outcome}_counts_ctrl_categ")]]
+    
+  }
+
+  orig_data <- data
+  
+  categorical_outcome <- outcome
+
+  data$outcome <- data[[outcome]]
+  data$measure <- data[[paste0(outcome, "_measure_categ")]]
+  data <- data %>% 
+    dplyr::filter(ai > 0 & ci > 0)  # Example: Ensure cells for RR are not zero
+  
+  
+  cat("Analyzing ", nrow(data), "studies.<p>")
+  if (nrow(data) == 0) {
+    cat("<p>No data.<p>")
+    return(NULL)
+  }
+  
+  
+  d_es <- metafor::escalc(
+    ai = ai,
+    bi = bi,
+    ci = ci,
+    di = di,
+    measure = "RR", slab = ID,
+    data = data,
+    append = TRUE
+  )
+  
+  d_es$comparator <- FALSE
+  
+  d_es <- d_es %>%
+    dplyr::filter(!is.na(yi))
+  
+  # Order by int_label (which is blank if no label) and then by ID
+  d_es <- d_es %>%
+    dplyr::arrange(int_label, ID)
+  
+  
+  # doing comparator analysis, calculate effect sizes
+  # and then join to d_es
+  if (comparator_analysis) {
+    data <- orig_data
+    
+    data$ai <- data[[glue::glue("{categorical_outcome}_counts_ctrl_categ")]]
+    data$bi <- 
+      data[[glue::glue("{categorical_outcome}_n_ctrl_categ")]] - 
+      data[[glue::glue("{categorical_outcome}_counts_ctrl_categ")]]
+    data$ci <-  data[[glue::glue("{categorical_outcome}_counts_comp_categ")]]
+    data$di <- data[[glue::glue("{categorical_outcome}_n_comp_categ")]] - 
+      data[[glue::glue("{categorical_outcome}_counts_comp_categ")]]
+    
+    #data$value <- data[[value]]
+    data$outcome <- data[[outcome]]
+    data$measure <- data[[paste0(outcome, "_measure_categ")]]
+    data <- 
+      data %>%
+      dplyr::filter(ai > 0 & ci > 0)  # Example: Ensure cells for RR are not zero
+    
+    if (nrow(data) > 0) {
+      d_comp_es <- metafor::escalc(
+        ai = ai,
+        bi = bi,
+        ci = ci,
+        di = di,
+        measure = "RR", slab = ID, data = data,
+        append = TRUE
+      ) %>%
+        dplyr::filter(!is.na(yi))
+      
+      d_comp_es <- d_comp_es %>%
+        dplyr::filter(
+          !(ID %in% d_es$ID)  # remove those that are already in ES
+        )
+      if (nrow(d_comp_es) > 0) {
+        d_comp_es$comparator <- TRUE # identify these as comparator
+        d_es <- dplyr::bind_rows(d_es, d_comp_es)
+        
+        d_es <- d_es %>% 
+          dplyr::mutate(comparator = ifelse(!is.na(comparator), TRUE, FALSE)
+          )
+        
+        d_es$ID <- ifelse(
+          d_es$comparator, 
+          glue::glue("{d_es$comparator} (C)"), 
+          d_es$comparator
+        )
+      }
+    }
+  }
+  
+  
+  # Reverse RR for substance use
+  if (outcome %in% 
+      c(
+        "substance_use", "relationship", "BP", "sleep_disturbance",
+        "AEs"
+      )
+  ) {
+    d_es <- d_es %>%
+      dplyr::mutate(yi = yi * -1, yi)
+    
+  }
+  
+  
+  if (nrow(d_es) > 0) {
+    d_es$ID <- paste0(d_es$ID, " ", d_es$int_label)
+  }
+  
+  cat("<p>Control Types <p>")
+  d_es %>% dplyr::group_by(control) %>%
+    dplyr::summarise(n = dplyr::n()) %>%
+    knitr::kable() %>%
+    print()
+  
+  if (nrow(d_es) > 0) {
+    fit_ma <- metafor::rma(
+      data = d_es, yi = yi, vi = vi,
+      slab = ID,test="knha"
+    )
+    d_es %>%
+      dplyr::mutate(se = sqrt(vi)) %>%
+      dplyr::select(ID, measure, yi, se) %>%
+      knitr::kable() %>%
+      print()
+    
+    forest(
+      fit_ma, atransf = exp, xlab = "Relative Risk", cex = 0.75, mlab = NULL,
+      fonts = "Arial"
+    )
+    
+    # Put thsi back if needed, and fix var names
+    #cat(
+    #  glue::glue(
+    #    "Number of studies = {nrow(d_es)}<p>
+    #  Intervention sample =  {sum(d_es[[n_int_categ]])} <p>
+    #  Control sample = {sum(d_es[[n_ctrl_categ]])}  <p>
+    #  Total sample = {sum(d_es[[n_int_categ]]) +
+    #  sum(d_es[[n_ctrl_categ]])}. <p>
+    #  "
+    #  )
+    #)
+    
+    cat("<p>relative Risk Estimate<p>")
+    coef(summary(fit_ma)) %>%
+      as.data.frame() %>%
+      #dplyr::select(estimate, ci.lb, ci.ub) %>%
+      round(2)%>%
+      exp() %>%
+      knitr::kable()
+    
+    cat("<p>I-squared = ", summary(fit_ma)$I2 %>% round(), "%<p>")
+    
+    if (nrow(d_es) > 2) {
+      cat("<h5>Publication Bias Tests</h5>")
+      cat("<h6>Begg (non-parametric)</h6>")
+      ranktest(fit_ma) %>% capture.output() %>% cat()
+      cat("<h6>Egger (parametric)</h6>")
+      cat(regtest(fit_ma) %>% capture.output(), sep = "\n" )
+      
+      
+      cat("<h6>Trim and Fill</h6>")
+      
+      # Line below breaks quarto
+      #metafor::trimfill(fit_ma) %>% print()
+      cat("Estimate and Cis<p>")
+      coef(summary(trimfill(fit_ma))) %>%
+        as.data.frame() %>%
+        dplyr::select(estimate, ci.lb, ci.ub) %>%
+        dplyr::mutate(
+          estimate = exp(estimate),
+          ci.lb = exp(ci.lb),
+          ci.ub = exp(ci.ub)
+        ) %>%
+        knitr::kable() %>% print()
+      cat("<p>TrimFill Relative Risk Estimate: ",
+          round(exp(coef(trimfill(fit_ma, stepadj=0.5, maxiter=10000)))), "<p>")
+      
+      cat("<h6>int_stimulant vs int_nonstimulant meta-regression</h6>")
+      
+      StimMetaReg(d_es)
+      
+    }
+    
+    
+    return(fit_ma)
+    
+    
+    
+  } else {
+    return("No data for meta-analysis.<p>")
+  }
+}
---
+++ contMA.R
@@ -0,0 +1,229 @@
+
+
+ContMA <- function(
+    data,
+    label = "ID",
+    outcome,
+    comparator_analysis = TRUE, 
+    comparator_vs_int = FALSE
+) {
+  
+  # Can't do both!
+  if (comparator_vs_int) {
+    comparator_analysis <- FALSE
+  }
+  
+  mean_ctrl_cont = glue::glue("{continuous_outcome}_mean_ctrl_cont")
+  SD_ctrl_cont = glue::glue("{continuous_outcome}_SD_ctrl_cont")
+  n_ctrl_cont = glue::glue("{continuous_outcome}_n_ctrl_cont")
+  mean_int_cont = glue::glue("{continuous_outcome}_mean_int_cont")
+  SD_int_cont = glue::glue("{continuous_outcome}_SD_int_cont")
+  n_int_cont = glue::glue("{continuous_outcome}_n_int_cont")
+  
+  # if comparator vs int, swap comp for ctrl
+  if (comparator_vs_int) {
+    mean_ctrl_cont = glue::glue("{continuous_outcome}_mean_comp_cont")
+    SD_ctrl_cont = glue::glue("{continuous_outcome}_SD_comp_cont")
+    n_ctrl_cont = glue::glue("{continuous_outcome}_n_comp_cont")
+  }
+  
+  
+  data$mean_ctrl_cont <- data[[mean_ctrl_cont]]
+  data$SD_ctrl_cont <- data[[SD_ctrl_cont]]
+  data$n_ctrl_cont <- data[[n_ctrl_cont]]
+  data$mean_int_cont <- data[[mean_int_cont]]
+  data$SD_int_cont <- data[[SD_int_cont]]
+  data$n_int_cont <- data[[n_int_cont]]
+  #data$value <- data[[value]]
+  data$outcome <- data[[outcome]]
+  data$measure <- data[[paste0(outcome, "_measure_cont")]]
+  data$value <- data[[paste0(outcome, "_value")]]
+  data$positive_direction <- data[[paste0(outcome, "_positive_direction")]]
+  
+  
+  orig_data <- data
+  
+  continuous_outcome <- outcome
+  
+  
+  data <- data %>%
+    dplyr::filter(
+      !is.na(mean_ctrl_cont) & !is.na(SD_ctrl_cont) & !is.na(n_ctrl_cont) &
+        !is.na(mean_int_cont) & !is.na(SD_int_cont) & !is.na(n_int_cont) &
+        SD_ctrl_cont > 0 & SD_int_cont > 0 &
+        n_ctrl_cont >= 2 & n_int_cont >= 2 # Basic conditions for SMD
+    ) 
+  
+  if (nrow(data) == 0) {
+    cat("<p>No data.<p>")
+    return(NULL)
+  }
+  d_es <- metafor::escalc(
+    m1i = mean_ctrl_cont,
+    sd1i = SD_ctrl_cont,
+    n1i = n_ctrl_cont,
+    m2i = mean_int_cont,
+    sd2i = SD_int_cont,
+    n2i = n_int_cont,
+    measure = "SMD", 
+    slab = data$ID, 
+    data = data,
+    append = TRUE
+  )
+  
+  d_es$comparator <- FALSE
+  
+  
+  # doing comparator analysis, calculate effect sizes
+  # and then join to d_es
+  if (comparator_analysis) {
+    data <- orig_data
+    data$mean_comp_cont <- data[[glue::glue("{continuous_outcome}_mean_comp_cont")]]
+    data$SD_comp_cont <- data[[glue::glue("{continuous_outcome}_SD_comp_cont")]]
+    data$n_comp_cont <- data[[glue::glue("{continuous_outcome}_n_comp_cont")]]
+    data$mean_ctrl_cont <- data[[mean_ctrl_cont]]
+    data$SD_ctrl_cont <- data[[SD_ctrl_cont]]
+    data$n_ctrl_cont <- data[[n_ctrl_cont]]
+    data$outcome <- data[[outcome]]
+    data$measure <- data[[paste0(outcome, "_measure_cont")]]
+    data$value <- data[[paste0(outcome, "_value")]]
+    data$positive_direction <- data[[paste0(outcome, "_positive_direction")]]
+    
+    
+    data <- data %>%
+      dplyr::filter(
+        !is.na(mean_ctrl_cont) & !is.na(SD_ctrl_cont) & !is.na(n_ctrl_cont) &
+          !is.na(mean_comp_cont) & !is.na(SD_comp_cont) & !is.na(n_comp_cont) &
+          SD_ctrl_cont > 0 & SD_comp_cont > 0 &
+          n_ctrl_cont >= 2 & n_comp_cont >= 2 # Basic conditions for SMD
+      ) 
+    
+    if (nrow(data) > 0) {
+      d_comp_es <- metafor::escalc(
+        m1i = mean_ctrl_cont,
+        sd1i = SD_ctrl_cont,
+        n1i = n_ctrl_cont,
+        m2i = mean_comp_cont,
+        sd2i = SD_comp_cont,
+        n2i = n_comp_cont,
+        measure = "SMD", 
+        slab = data$ID, 
+        data = data,
+        append = TRUE
+      ) 
+      d_comp_es$comparator <- TRUE # identify these as comparator
+      d_comp_es <- d_comp_es %>%
+        dplyr::filter(
+          !(ID %in% d_es$ID)  # remove those that are already in ES
+        )
+      d_es <- dplyr::bind_rows(d_es, d_comp_es)
+      
+      
+      d_es <- d_es %>% 
+        dplyr::mutate(comparator = ifelse(!is.na(comparator), TRUE, FALSE)
+        )
+      
+      
+      d_es$ID <- ifelse(
+        d_es$comparator == TRUE, 
+        paste0(d_es$ID, " (C)"),
+        d_es$ID
+      )
+    }
+  }
+  
+  if (nrow(d_es) > 0) {
+    d_es$ID <- paste0(d_es$ID, " ", d_es$int_label)
+  }
+  # Reverse continuous variables so that lower is always better
+  
+  d_es <- d_es %>%
+    dplyr::mutate(
+      reverse = (positive_direction == "higher" &
+                   value == "Lower is better") | 
+        (positive_direction == "lower" &
+           value == "Higher is better"),
+      yi = ifelse(
+        reverse,
+        d_es$yi * -1, d_es$yi
+      )
+    ) 
+  
+  
+  # Order by int_label (which is blank if no label) and then by ID
+  d_es <- d_es %>%
+    dplyr::arrange(int_label, ID)
+  
+  if (nrow(d_es) == 0) {
+    cat("No data to analyze")
+    return(NULL)
+  }
+  
+  if (nrow(d_es) > 0) {
+    
+    fit_ma <- metafor::rma(
+      data = d_es,
+      yi = yi,
+      vi = vi,
+      slab = ID, test = "knha")
+    cat("Main Meta Analysis Result<p>")
+    
+    d_es %>%
+      dplyr::mutate(se = sqrt(vi)) %>%
+      dplyr::select(
+        ID, measure,  
+        mean_ctrl_cont, SD_ctrl_cont, n_ctrl_cont, 
+        mean_int_cont, SD_int_cont, n_int_cont, 
+        yi, se) %>%
+      knitr::kable() %>% print()
+    
+    forest(
+      fit_ma, xlab = "Standardized Mean Difference", 
+      cex = 0.75, mlab = NULL, fonts = "Arial",
+      main = continuous_outcome
+    )
+    
+    
+    cat(
+      glue::glue(
+        "Number of studies = {nrow(d_es)}<p>
+      Intervention sample =  {sum(d_es$n_int_cont)}<p>
+      Control sample = {sum(d_es$n_ctrl_cont)} <p>
+      Total sample = {sum(d_es$n_int_cont) + sum(d_es$n_ctrl_cont)}.<p>"
+      )
+    )
+    
+    
+    coef(summary(fit_ma)) %>%
+      as.data.frame() %>%
+      # dplyr::select(estimate, ci.lb, ci.ub) %>%
+      round(2)%>%
+      knitr::kable()
+    
+    cat("<p>I-squared = ", summary(fit_ma)$I2 %>% round(), "% <p>")
+    
+    if (nrow(d_es) > 2) {
+      cat("<h5>Publication Bias Tests</h5>")
+      cat("<h6>Begg (non-parametric)</h6>")
+      ranktest(fit_ma) %>% capture.output() %>% cat()
+      cat("<h6>Egger (parametric)</h6>")
+      cat(regtest(fit_ma) %>% capture.output(), sep = "\n" )
+      
+      
+      cat("<h6>Trim and Fill</h6>")
+      
+      # Line below breaks quarto
+      #metafor::trimfill(fit_ma) %>% print()
+      cat("Estimate and Cis<p>")
+      coef(summary(trimfill(fit_ma))) %>%
+        as.data.frame() %>%
+        dplyr::select(estimate, ci.lb, ci.ub) %>%
+        round(2) %>%
+        knitr::kable() %>% print()
+      
+      StimMetaReg(d_es)
+      
+    }
+  } 
+}
+
